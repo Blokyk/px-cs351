1eed6a7bec0a294182613d609c348d057d4c5730
blokyk <eliot.courvoisier@gmail.com>
2024-01-23 00:10:41 +0100

    nit: parenthesize some macro operands

--- common/utils.h
+++ common/utils.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define mask_bits(start, end) ((((uint32_t)~0 >> start) << (31-(end-start))) >> (31-end))
-#define get_bits(val, start, end) ((((uint32_t)val) & mask_bits(start, end)) >> start)
+#define mask_bits(start, end) ((((uint32_t)~0 >> (start)) << (31-((end)-(start)))) >> (31-(end)))
+#define get_bits(val, start, end) ((((uint32_t)val) & mask_bits(start, end)) >> (start))
 #define get_bit(val, idx) get_bits(val, idx, idx)
